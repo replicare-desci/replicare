7fe30b50e39f37d2ce80bb1cb0ac03aa16d099d5
HARSH MITTAL <harshmittal1750@gmail.com>
2023-04-07 18:03:42 +0530

    updated ui

--- src/Components/Binder/Navbar.tsx
+++ src/Components/Binder/Navbar.tsx
@@ -2,28 +2,48 @@ import Sidebar from "../Sidebar";
 // import ConnectionWallet from "../ConnectionWallet";
 import { Link } from "react-router-dom";
 import ConnectWalletPopUp from "../ConnectWalletPopUp";
-import { AppBar, Toolbar, Typography } from "@mui/material";
+import {
+  AppBar,
+  Box,
+  Button,
+  IconButton,
+  Toolbar,
+  Typography,
+} from "@mui/material";
 import logo from "../../assets/images/logo.png";
 const Navbar = () => {
   return (
     <div>
       <AppBar position="relative">
-        <Toolbar sx={{ justifyContent: "space-between" }}>
-          <Sidebar />
+        <Toolbar>
           {/* <Button sx={{ color: "#ffffff" }}>Signup</Button> */}
-          <Link to="/" style={{ textDecoration: "none", color: "#222629" }}>
-            <Typography
-              variant="h4"
-              color="inherit"
-              noWrap
-              justifyContent={"center"}
-            >
-              Replicare
-            </Typography>
-            <img style={{ width: "10%" }} src={logo} alt="logo" />
+          <Link to="/">
+            <Box display={"flex"}>
+              {" "}
+              <img style={{ width: "15%" }} src={logo} alt="logo" />
+            </Box>
           </Link>
           {/* <ConnectionWallet /> */}
-          <ConnectWalletPopUp />
+          <Box
+            display={"flex"}
+            justifyContent={"flex-end"}
+            sx={{ width: "100%" }}
+          >
+            <Link to="https://docs.replicare.dev">
+              {" "}
+              <Button
+                sx={{
+                  color: "primary.main",
+                  backgroundColor: "background.paper",
+                  mx: 2,
+                }}
+                variant="contained"
+              >
+                Docs
+              </Button>
+            </Link>
+            <ConnectWalletPopUp /> <Sidebar />
+          </Box>
         </Toolbar>
       </AppBar>
     </div>
