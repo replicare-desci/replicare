bb5944f7f33f18a5f5dffb398b346a5c5be2931c
HARSH MITTAL <harshmittal1750@gmail.com>
2023-04-07 23:38:55 +0530

    bugs fixes , improved code

--- src/types/context.d.ts
+++ src/types/context.d.ts
@@ -16,6 +16,7 @@ export interface ContextInterface {
 }
 
 export type formDataType = {
+  checkBoxData: boolean[];
   reproductionPackageAvailable: boolean;
   authorContacted: boolean;
   // authorAvailableForFurtherQuestion: boolean,
