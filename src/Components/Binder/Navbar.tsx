1d0a151e30b85999b60fc7d9907557d702219659
HARSH MITTAL <harshmittal1750@gmail.com>
2023-04-11 00:11:21 +0530

    small screen ui fix

--- src/Components/Binder/Navbar.tsx
+++ src/Components/Binder/Navbar.tsx
@@ -57,7 +57,7 @@ const Navbar = () => {
                 }}
                 variant="contained"
               >
-                <ArticleIcon sx={{ fontSize: 18, mr: 1 }} />
+                <ArticleIcon sx={{ fontSize: 18, mr: 0.5 }} />
                 Docs
               </Button>
             </Link>
